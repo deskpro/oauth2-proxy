34e9aa936012579171d133c5409be5d3dee4fa1f
radu <radu.helstern@deskpro.com>
2018-10-04 10:05:58 +0100

    allow overwritting the docker-compose settings with a local file add missing files into the nginx dockerfile change the redirect url to /social/authenticate-callback..

--- bin/start.sh
+++ bin/start.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-(cd src/docker-compose && COMPOSE_PROJECT_NAME=oauth2-proxy docker-compose up )
+if [ -f src/docker-compose/docker-compose.dev.yml ]; then
+    echo running with dev configuration
+    (cd src/docker-compose && COMPOSE_PROJECT_NAME=oauth2-proxy docker-compose -f docker-compose.yml -f docker-compose.dev.yml up )
+else
+    echo running with prod configuration
+    (cd src/docker-compose && COMPOSE_PROJECT_NAME=oauth2-proxy docker-compose -f docker-compose.yml -f docker-compose.prod.yml up )
+fi
+
